e05df6189da150391d4c91b962c9eb54bc8300a8
Seif El-Din Sweilam <saifsweelam@gmail.com>
2019-11-15 16:43:45 +0200

    Define Category & Photo Classes

--- db_setup.py
+++ db_setup.py
@@ -5,3 +5,35 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
  
 Base = declarative_base()
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name
+        }
+
+class Photo(Base):
+    __tablename__ = 'scholar'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(250), nullable=False)
+    description = Column(String(360))
+    url = Column(String, nullable=False)
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'description': self.description,
+            'url': self.url
+        }
