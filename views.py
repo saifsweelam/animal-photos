8967ff1619906fe1f6dd1afa705fbc3b2867eab0
Seif El-Din Sweilam <saifsweelam@gmail.com>
2019-11-18 23:41:13 +0200

    Make logout returns to homepage

--- views.py
+++ views.py
@@ -172,8 +172,8 @@ def gdisconnect():
         del login_session['email']
         del login_session['picture']
 
-        response = make_response(json.dumps('Successfully disconnected.'), 200)
-        response.headers['Content-Type'] = 'application/json'
+        response = make_response(json.dumps('Successfully disconnected.'), 301)
+        response.headers['Location'] = '/'
         return response
     else:
         # For whatever reason, the given token was invalid.
